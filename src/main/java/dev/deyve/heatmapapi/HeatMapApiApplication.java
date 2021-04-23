5e0f948c2435762662d8e8783e06c7064f4a4b58
Deyve Machado <deyvedvm@hotmail.com>
2021-04-23 15:57:23 -0300

    Initial commit

---
+++ src/main/java/dev/deyve/heatmapapi/HeatMapApiApplication.java
@@ -0,0 +1,13 @@
+package dev.deyve.heatmapapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeatMapApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeatMapApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/deyve/heatmapapi/HeatMapApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.deyve.heatmapapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeatMapApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
